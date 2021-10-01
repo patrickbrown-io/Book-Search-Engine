c3673bfe167735c27247108967a149b8dd9884d6
Patrick Brown <breepatbree@gmail.com>
2021-10-01 09:11:45 -0600

    Added Apollo and Express Server as middleware for server.js

--- server/server.js
+++ server/server.js
@@ -3,19 +3,30 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+// Bring in Apollo Server
+const { ApolloServer } = require("apollo-server-express");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//  Apollo
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+// Apply Apollo as Middleware
+server.applyMiddleware({app});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 app.use(routes);
 
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
